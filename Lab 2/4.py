ec7c5f5de8e7c144f4b3c5e9f662235564aa12d5
Dinura Hansajith <99352648+IT21231650@users.noreply.github.com>
2026-03-17 14:37:29 +0530

    Create 4.py

---
+++ Lab 2/4.py
@@ -0,0 +1,16 @@
+name = input(" Enter Name - ")
+school =input(" Enter School - ")
+
+
+#My Name And School
+print("\n" + name + "\n" + school)
+
+"""
+We using comments because , if there will be any issue come up with code,
+programmer can easily understand the parts in this
+"""
+
+
+
+
+
